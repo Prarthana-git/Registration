12c3e36fa5d3e9dc15ca49e53839f541efe3cfc4
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-16 20:31:04 +0530

    Added test cases

--- src/main/java/com/bidgelabz/Registration.java
+++ src/main/java/com/bidgelabz/Registration.java
@@ -5,62 +5,59 @@ import java.util.regex.Pattern;
 public class Registration {
     String firstName, lastName,email,phoneNumber;
     boolean check;
-    Scanner scan = new Scanner(System.in);
 
-    public void checkFirstName() {
+    public boolean checkFirstName(String name) {
         System.out.println("Enter First-Name starts with capital letter and has minimum 3 character");
-        firstName = scan.nextLine();
+        firstName = name;
         check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
         if (!check) {
             System.out.println("Invalid!!Please Enter your name according to the instruction");
-            checkFirstName();
+            checkFirstName(name);
         } else {
             System.out.println("It's a Valid Firstname");
         }
+        return check;
     }
 
     //checko lastname method will check the user input according to the given pattern
-    public void checkLastName() {
+    public boolean checkLastName(String name) {
         System.out.println("Enter LastName starts with capital letter and has minimum 3 character");
-        lastName = scan.nextLine();
+        lastName = name;
         check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
         if (!check) {
             System.out.println("Invalid!!!");
-            checkLastName();
-        } else
+            checkLastName(name);
+        } else{
             System.out.println("It is a valid lastname");
+    } return check;
     }
 
     //added email method to match the email ids according to given pattern
-    public void checkEmail()
+    public boolean checkEmail(String mail)
     {
         System.out.println("Enter a valid email");
-        email=scan.nextLine();
+        email=mail;
         check=Pattern.compile("^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,}+)*$").matcher(email).matches();
-        if (!check) {    System.out.println(" Invalid!!Email-id...Please Enter a Valid one. ");
-            checkEmail();
+        if (!check) {
+            System.out.println(" Invalid!!Email-id...Please Enter a Valid one. ");
+            checkEmail(mail);
         }
         else {
             System.out.println(" Email-Id is valid ");
         }
-    }public void checkPhoneNumber()
+        return check;
+    }
+    public boolean checkPhoneNumber(String no)
     {System.out.println("Enter Your Mobile Number");
-        phoneNumber = scan.nextLine();
+        phoneNumber = no;
         check=Pattern.compile("^[0-9]{1,3} [0-9]{10}$").matcher(phoneNumber).matches();
         if (!check) {
             System.out.println(" Number is invalid!!Please Enter a Valid one. ");
-            checkPhoneNumber();
+            checkPhoneNumber(no);
         }
         else {
             System.out.println("Valid Number ");
         }
+        return check;
     }
-
-    public static void main (String[] args)
-    {
-        Registration reg=new Registration();
-        reg.checkFirstName();
-        reg.checkLastName();
-        reg.checkEmail();
-        reg.checkPhoneNumber();}
 }
---
+++ src/test/java/com/bidgelabz/RegistrationTest.java
@@ -0,0 +1,38 @@
+package com.bidgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class RegistrationTest {
+    @Test
+    public void firstnameShouldbeasspecify()
+    {
+        Registration reg=new Registration();
+        String name="Prarthana";
+        Assert.assertEquals(true,reg.checkFirstName(name));
+
+    }
+    @Test
+    public void lastnameShouldbeasspecify()
+    {
+        Registration reg=new Registration();
+        String name="Chaudhari";
+        Assert.assertEquals(true,reg.checkLastName(name));
+    }
+    @Test
+    public void emailShouldbeasspecify()
+    {
+        Registration reg=new Registration();
+        String mail="abc.xyz@bl.co.in";
+        Assert.assertEquals(true,reg.checkEmail(mail));
+    }
+    @Test
+    public void phoneShouldbeasspecify()
+    {
+        Registration reg=new Registration();
+        String no="91 9754856458";
+        Assert.assertEquals(true,reg.checkPhoneNumber(no));
+    }
+}
